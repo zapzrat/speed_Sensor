1f0d1d150358cd7d29938298ef315c898499464a
zapzrat <zapzrat@gmail.com>
2021-11-09 20:48:39 +0700

    Add files via upload

---
+++ Speed_Sensor.ino
@@ -0,0 +1,31 @@
+const int speedSensorPin = 2 ;
+const int ledPin = D0;
+
+int statusSpeed = 0;
+int c = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Micro Projects th");
+  pinMode(ledPin, OUTPUT);
+  pinMode(speedSensorPin, INPUT);
+}
+
+void loop() {
+  statusSpeed = digitalRead(speedSensorPin);
+
+  if (statusSpeed == HIGH) {
+    digitalWrite(ledPin, HIGH);
+
+    c++;
+    if (c == 1) {
+      Serial.println("************ Start ***********");
+    }
+    Serial.print(c);
+    Serial.println(" : Led HIGH");
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+    c = 0;
+  }
+}
